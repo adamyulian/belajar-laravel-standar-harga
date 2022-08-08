081d0ee2e3c2686b07fe863b81a7c6b5a0d8e2b0
adamyulian <aadamyulian@gmail.com>
2022-08-08 22:44:31 +0700

    coba menambahkan docker

--- resources/views/shs/index.blade.php
+++ resources/views/shs/index.blade.php
@@ -32,7 +32,7 @@
                                 <td>{{$standar_hargas->kode_komp}}</td>
                                 <td>{{$standar_hargas->nama_komp}}</td>
                                 <td>{{$standar_hargas->spesifikasi}}</td>
-                                <td>{{$standar_hargas->satuan->satuan}}</td>
+                                <td>{{$standar_hargas->satuan->id}}</td>
                                 <td class="text-right">{{$standar_hargas->harga_satuan}}</td>
                                 <td>{{$standar_hargas->pajak}}</td>
                                 <td>{{$standar_hargas->kodefikasi_rekening_belanja->kode_rekening_belanja}}</td>
