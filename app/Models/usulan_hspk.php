e6812f17590993b4f3eb5b7cfe4e8fa7f358fc0d
adamyulian <aadamyulian@Gmail.com>
2022-08-10 18:39:53 +0700

    model hspk belum lengkap untuk relasi antar tabel yang sudah ada

--- app/Models/usulan_hspk.php
+++ app/Models/usulan_hspk.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class usulan_hspk extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+            'standar_harga_id',
+            'satuan_id',
+            'kode_hspk',
+            'nama_hspk',
+            'koefisien_harga_hspk',
+            'jumlah_bahan_hspk',
+            'jumlah_tenaga_hspk',
+            'jumlah_peralatan_hspk',
+            'jumlah_total_hspk',
+            'overheadprofit_hspk',
+            'keterangan',
+            'harga_hspk',
+            'koefisien',
+        ];
+
+        public function kodefikasi_aset()
+            {
+            return $this->belongsTo(kodefikasi_aset::class);
+            }
 }
